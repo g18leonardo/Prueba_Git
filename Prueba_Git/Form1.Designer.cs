070bef2d3cd38e18643f1119b0fdea0399a87761
g18leonardo <g18leonardo@hotmail.com>
2023-05-02 19:16:45 -0500

    Cambios mas

--- Prueba_Git/Form1.Designer.cs
+++ Prueba_Git/Form1.Designer.cs
@@ -64,7 +64,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(376, 85);
+            this.button2.Location = new System.Drawing.Point(264, 258);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 3;
